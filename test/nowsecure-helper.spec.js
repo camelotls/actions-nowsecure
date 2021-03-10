b153b2d3c4b9211f1d6bee67cc814128053d86e6
Georgios Romanas <giorgos.romanas@camelotls.com>
2021-03-10 18:43:33 +0200

    [update-unit-test] - add unit test for nowsecure-helpers.js

--- test/nowsecure-helper.spec.js
+++ test/nowsecure-helper.spec.js
@@ -34,7 +34,7 @@ describe('Nowsecure REST calls are functioning properly', () => {
     it('An assessment can be retrieved for iOS', async () => {
       nock(`https://${config.NOWSECURE.URI}`)
         .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/ios/${mock.MOCK_APPLICATION_IOS_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/?group=${mock.MOCK_GROUP_ID}`)
-        .reply(200, mock.MOCK_IOS_ASSESSMENT_REPORT);
+        .reply(200, mock.MOCK_ASSESSMENT);
 
       const assessment = await nowsecure.retrieveAssessment('ios');
       expect(assessment[0]).to.be.instanceOf(Object).to.have.all.keys('ref',
@@ -60,7 +60,7 @@ describe('Nowsecure REST calls are functioning properly', () => {
     it('An assessment can be retrieved for Android', async () => {
       nock(`https://${config.NOWSECURE.URI}`)
         .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/android/${mock.MOCK_APPLICATION_ANDROID_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/?group=${mock.MOCK_GROUP_ID}`)
-        .reply(200, mock.MOCK_ANDROID_ASSESSMENT_REPORT);
+        .reply(200, mock.MOCK_ASSESSMENT);
 
       const assessment = await nowsecure.retrieveAssessment('android');
       expect(assessment[0]).to.be.instanceOf(Object).to.have.all.keys('ref',
@@ -82,6 +82,62 @@ describe('Nowsecure REST calls are functioning properly', () => {
         'adjusted_issues',
         'adjusted_score',
         'identified_vuln_map');
+    });
+    it('Assessment results can be retrieved for iOS', async () => {
+      nock(`https://${config.NOWSECURE.URI}`)
+        .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/ios/${mock.MOCK_APPLICATION_IOS_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${mock.MOCK_IOS_TASK}/results?group=${mock.MOCK_GROUP_ID}`)
+        .reply(200, mock.MOCK_IOS_ASSESSMENT_REPORT_PER_TASK);
+
+      const assessmentResults = await nowsecure.retrieveAssessmentResults('ios', mock.MOCK_IOS_TASK);
+      expect(assessmentResults[0]).to.be.instanceOf(Object).to.have.all.keys('yaap_filtered',
+        'yaap',
+        'static',
+        'dynamic');
+    });
+    it('Assessment results can be retrieved for Android', async () => {
+      nock(`https://${config.NOWSECURE.URI}`)
+        .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/android/${mock.MOCK_APPLICATION_ANDROID_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${mock.MOCK_ANDROID_TASK}/results?group=${mock.MOCK_GROUP_ID}`)
+        .reply(200, mock.MOCK_IOS_ASSESSMENT_REPORT_PER_TASK);
+
+      const assessmentResults = await nowsecure.retrieveAssessmentResults('android', mock.MOCK_ANDROID_TASK);
+      expect(assessmentResults[0]).to.be.instanceOf(Object).to.have.all.keys('yaap_filtered',
+        'yaap',
+        'static',
+        'dynamic');
+    });
+    it('Assessment report can be retrieved for iOS', async () => {
+      nock(`https://${config.NOWSECURE.URI}`)
+        .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/ios/${mock.MOCK_APPLICATION_IOS_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${mock.MOCK_IOS_TASK}/report?group=${mock.MOCK_GROUP_ID}`)
+        .reply(200, mock.MOCK_IOS_ASSESSMENT_RESULT);
+
+      const assessmentResults = await nowsecure.retrieveAssessmentReport('ios', mock.MOCK_IOS_TASK);
+      expect(assessmentResults[0]).to.be.instanceOf(Object).to.have.all.keys('key',
+        'kind',
+        'title',
+        'category',
+        'summary',
+        'cvss',
+        'cvss_vector',
+        'regulatory',
+        'affected',
+        'severity');
+    });
+    it('Assessment report can be retrieved for Android', async () => {
+      nock(`https://${config.NOWSECURE.URI}`)
+        .get(`/${config.NOWSECURE.ENDPOINTS.APPLICATION}/android/${mock.MOCK_APPLICATION_ANDROID_PACKAGE}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${mock.MOCK_ANDROID_TASK}/report?group=${mock.MOCK_GROUP_ID}`)
+        .reply(200, mock.MOCK_IOS_ASSESSMENT_RESULT);
+
+      const assessmentResults = await nowsecure.retrieveAssessmentReport('android', mock.MOCK_ANDROID_TASK);
+      expect(assessmentResults[0]).to.be.instanceOf(Object).to.have.all.keys('key',
+        'kind',
+        'title',
+        'category',
+        'summary',
+        'cvss',
+        'cvss_vector',
+        'regulatory',
+        'affected',
+        'severity');
     });
   });
 });

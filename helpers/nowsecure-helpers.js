183ab94e559ace71b4fb947e5820b5ace559da44
panagiotis megremis <panagiotis.megremis@camelotls.com>
2021-03-10 13:36:47 +0200

    [nowsecure-update-group-id] update ReadMe file and adding group_id param in the retrieveAssessmentReport and retrieveAssessmentResults

--- helpers/nowsecure-helpers.js
+++ helpers/nowsecure-helpers.js
@@ -15,7 +15,7 @@ const retrieveAssessment = async function retrieveAssessment (platform) {
 
 const retrieveAssessmentResults = async function retrieveAssessmentResults (platform, task) {
   const appPackage = platform === 'android' ? config.APPLICATION.PACKAGES.ANDROID : config.APPLICATION.PACKAGES.IOS;
-  const nowsecureEndpoint = `/${config.NOWSECURE.ENDPOINTS.APPLICATION}/${platform}/${appPackage}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${task}/${config.NOWSECURE.ENDPOINTS.RESULTS}`;
+  const nowsecureEndpoint = `/${config.NOWSECURE.ENDPOINTS.APPLICATION}/${platform}/${appPackage}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${task}/${config.NOWSECURE.ENDPOINTS.RESULTS}/?group=${config.NOWSECURE.GROUP_ID}`;
   const response = await rest.GETRequestWrapper(retrieveAssessmentResults.name, config.NOWSECURE.URI, config.NOWSECURE.ACCESS_TOKEN, nowsecureEndpoint, true);
 
   assert(response.statusCode === 200, `Assessment's results cannot be retrieved for platform ${platform}: ${response.body.message}`);
@@ -25,7 +25,7 @@ const retrieveAssessmentResults = async function retrieveAssessmentResults (plat
 
 const retrieveAssessmentReport = async function retrieveAssessmentReport (platform, task) {
   const appPackage = platform === 'android' ? config.APPLICATION.PACKAGES.ANDROID : config.APPLICATION.PACKAGES.IOS;
-  const nowsecureEndpoint = `/${config.NOWSECURE.ENDPOINTS.APPLICATION}/${platform}/${appPackage}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${task}/${config.NOWSECURE.ENDPOINTS.REPORT}`;
+  const nowsecureEndpoint = `/${config.NOWSECURE.ENDPOINTS.APPLICATION}/${platform}/${appPackage}/${config.NOWSECURE.ENDPOINTS.ASSESSMENT}/${task}/${config.NOWSECURE.ENDPOINTS.REPORT}/?group=${config.NOWSECURE.GROUP_ID}`;
   const response = await rest.GETRequestWrapper(retrieveAssessmentResults.name, config.NOWSECURE.URI, config.NOWSECURE.ACCESS_TOKEN, nowsecureEndpoint, true);
 
   assert(response.statusCode === 200, `Assessment's report cannot be retrieved for platform ${platform}: ${response.body.message}`);

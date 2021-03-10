cc6310fa5d13ce077cc9cbe30c3859b0b3277eda
Georgios Romanas <giorgos.romanas@camelotls.com>
2021-03-10 16:43:31 +0200

    [update-unit-test] - add missing env variables from unit tests

--- config/config.js
+++ config/config.js
@@ -6,19 +6,21 @@ const NOWSECURE = {
     ASSESSMENT: 'assessment',
     APPLICATION: 'app',
     RESULTS: 'results',
-    REPORT: 'report',
+    REPORT: 'report'
   },
   ACCESS_TOKEN: core.getInput('NOWSECURE_ACCESS_TOKEN') || process.env.NOWSECURE_ACCESS_TOKEN,
   GROUP_ID: core.getInput('NOWSECURE_GROUP_ID') || process.env.NOWSECURE_GROUP_ID
-}
+};
+
 const REST = {
   HEADER_ACCEPT_APPLICATION_JSON: 'application/json'
-}
+};
+
 const APPLICATION = {
   PACKAGES: {
     IOS: core.getInput('IOS_PACKAGE') || process.env.IOS_PACKAGE,
     ANDROID: core.getInput('ANDROID_PACKAGE') || process.env.ANDROID_PACKAGE
   }
-}
+};
 
 module.exports = { NOWSECURE: NOWSECURE, REST: REST, APPLICATION: APPLICATION }

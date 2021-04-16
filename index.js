39c7341a2786d4a79c0a20b8315e7b80d9cee1cb
panagiotis megremis <panagiotis.megremis@camelotls.com>
2021-04-16 14:06:18 +0300

    fix(): Handle incomplete assessments
    Comment in core.setOutput
    
    Contributes to #59

--- index.js
+++ index.js
@@ -159,8 +159,7 @@ const startAnalysis = async () => {
   });
 
   // output the constructed object
-  // core.setOutput('nowsecureReportData', reportOutput);
-  console.log(reportOutput);
+   core.setOutput('nowsecureReportData', reportOutput);
 };
 
 (async () => {

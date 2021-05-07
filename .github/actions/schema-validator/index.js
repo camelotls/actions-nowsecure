fa5e29fd58690aa06377464fe01dc6281465063a
panagiotis megremis <panagiotis.megremis@camelotls.com>
2021-05-07 18:06:12 +0300

    feat(): Add Integration tests Update index.js Contributes to: #84

--- .github/actions/schema-validator/index.js
+++ .github/actions/schema-validator/index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
 const dirtyJSON = require('dirty-json');
 const Validator = require('jsonschema').Validator;
+const schemaType = core.getInput('ID') || process.env.ID;
 const inputData = core.getInput('INPUT_DATA') || process.env.INPUT_DATA;
-const  {nowSecureSchema, nowSecureExtraFieldsSchema } = require('schemaTemplate');
+const { nowSecureSchema, nowSecureExtraFieldsSchema } = require('./schemaTemplate');
 const schemaArray = [];
 
 schemaArray.push(nowSecureSchema, nowSecureExtraFieldsSchema);
 
 schemaArray.forEach( schema => {
-  if(dirtyJSON.parse(schema.id) === process.env.ID) {
-    console.log(process.env.ID)
+
+  if(dirtyJSON.parse(schema.id) === schemaType) {
 
     const jsonValidator = new Validator();
     const beautifiedInputData = dirtyJSON.parse(inputData);
@@ -20,14 +21,16 @@ schemaArray.forEach( schema => {
       const isValidSchema = jsonValidator.validate(JSON.parse(beautifiedInputDataStringified), schema).errors.length === 0;
 
       if (isValidSchema) {
-        console.log('NowSecure schema validation has succeeded.')
+        console.log('NowSecure schema validation has succeeded.');
       } else {
         console.log('Validation of Nowsecure schema has failed. ' + jsonValidator.validate(JSON.parse(beautifiedInputDataStringified), schema));
-        process.exit(1)
+        process.exit(1);
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
+  } else {
+    console.log('Schema Type is not valid')
   }
 });
 

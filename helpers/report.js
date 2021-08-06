d3bd2c59f547c1738f05dc036ef2f50651caa202
Stelios Gkiokas <stelios.giokas@camelotls.com>
2021-08-06 14:08:11 +0300

    feat: Populating the NowSecure pdf report (#135)
    This PR adds the ability to generate the pdf report per platform for the latest assessment retrieved from the action. Currently, the report(s) will reside in the filesystem on which the action will run on. In an upcoming PR, the ability to export that report to a chained action will be added.
    
    Contributes to: camelotls/actions-nowsecure#130
    
    Signed-off-by: Stelios Gkiokas <stelios.giokas@camelotls.com>

---
+++ helpers/report.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({ name: 'actions-nowsecure' });
+
+const nowSecure = require('./nowsecure-helpers');
+const utils = require('./utils');
+const { NOWSECURE } = require('../config/config');
+
+const getAssessments = async (platforms, extractReport) => {
+  const assessments = [];
+
+  for (const platform of platforms) {
+    log.info(`Retrieving the assessment for platform ${platform}...`);
+    const assessment = await nowSecure.retrieveAssessment(platform);
+    assessments.push(assessment);
+
+    if (assessment !== null || assessment !== undefined) {
+      log.info(`The assessment for platform ${platform} was retrieved successfully!`);
+    }
+  }
+
+  return assessments;
+};
+
+const generateReport = (platform, assessmentReference) => {
+  utils.shellExec(`curl -o ${platform}.pdf -H 'Authorization: Bearer ${NOWSECURE.ACCESS_TOKEN}' 'https://lab-api.nowsecure.com/assessment/${assessmentReference}/report.pdf'`);
+};
+
+module.exports = { getAssessments, generateReport };
